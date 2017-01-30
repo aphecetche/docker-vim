a6670ddc2d0572ead522c604d96473b13d8a846a
laphecet <laurent.aphecetche@cern.ch>
2017-01-30 14:00:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Base image of vim 8.0 with a bunch of plugins, in particular YouCompleteMe configured for C++
---
+++ vim.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+BASE16_SHELL=/root/base16-shell
+
+# do not use the profile_helper script as it does dot fare well with ash shell
+# but do here just what we need
+# [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+
+script=$BASE16_SHELL/scripts/$BASE16_THEME.sh
+
+echo "script=$script"
+
+if test -f $script; then
+	. $script
+	#eval $script
+	echo -e "if \0041exists('g:colors_name') || g:colors_name != '$BASE16_THEME'\n  colorscheme $BASE16_THEME\nendif" >| ~/.vimrc_background
+
+	cat ~/.vimrc_background
+fi
+
+vim "$@"
+
